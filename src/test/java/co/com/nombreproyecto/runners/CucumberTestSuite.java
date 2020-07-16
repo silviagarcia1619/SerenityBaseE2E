53c622c6cd225f6875a56bb5abfba8337dcf632c
juliana <silviasamuel19@hotmail.com>
2020-07-15 20:17:14 -0500

    ajuste en runner

--- src/test/java/co/com/nombreproyecto/runners/CucumberTestSuite.java
+++ src/test/java/co/com/nombreproyecto/runners/CucumberTestSuite.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
     features = "src/test/resources/features/",
     glue = "co.com.nombreproyecto.stepsdefinitions",
 //    tags = "not @example",
-    tags = {"@google"},
+    tags = {"@github"},
     snippets = SnippetType.CAMELCASE
 
 )
